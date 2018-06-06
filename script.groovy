dba3e165d05360856d44ebfa85e3a26524c661a2
sandipauti11 <sandip.auti11@gmail.com>
2018-06-06 14:25:52 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,11 @@
-
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('https://github.com/sandipauti11/job-dsl-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
